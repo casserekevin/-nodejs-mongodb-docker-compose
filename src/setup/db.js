3f7eddae7d9458aa9f404392b7e7080835e90365
casserekevin <kevin_cassere13@hotmail.com>
2021-01-15 13:27:04 -0300

    final example

---
+++ src/setup/db.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+
+const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env
+
+mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`, {
+    useNewUrlParser: true
+})
+
+mongoose.connection.on('error', () => console.error('database connection ERROR!'))
+mongoose.connection.once('open', () => console.log('database connection OK!'))
